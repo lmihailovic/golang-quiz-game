d39dc8abd86c4964c6b28649a56dd63bc5f65161
lmihailovic <miha.luka1006@gmail.com>
2025-08-02 20:25:07 +0200

    added game timer

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func ReadCSV(filename string) [][]string {
@@ -26,12 +27,25 @@ func ReadCSV(filename string) [][]string {
 	return records
 }
 
+func EndGame(score int, totalProblems int) {
+	fmt.Printf("\nYour scored: %v out of %v\n", score, totalProblems)
+	os.Exit(0)
+}
+
 func main() {
-	problemsFile := flag.String("file", "problems.csv", "path to problems file")
+	problemsFile := flag.String("f", "problems.csv", "path to problems file")
+	gameTime := flag.Int("t", 30, "time to play in seconds")
 	flag.Parse()
 
 	records := ReadCSV(*problemsFile)
 	score := 0
+
+	fmt.Println("Press any key to start.")
+	fmt.Scanln()
+
+	time.AfterFunc(time.Duration(*gameTime)*time.Second, func() {
+		EndGame(score, len(records))
+	})
 
 	for i, strings := range records {
 		fmt.Printf("Problem #%d: %v = ", i+1, strings[0])
@@ -47,5 +61,5 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Your scored: %v out of %v\n", score, len(records))
+	EndGame(score, len(records))
 }
